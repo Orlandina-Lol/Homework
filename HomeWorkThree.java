3b9a4e09d1e1b8e7d1a7bd8b29a68c27b4c920a4
Marina <orlandina.fem@gmail.com>
2021-12-20 12:52:26 +0400

    updated HomeworkThree.java

--- HomeWorkThree.java
+++ HomeWorkThree.java
@@ -16,37 +16,27 @@ class HomeWorkThree {
         int[] mass = retArr(12, 5);
         System.out.println(Arrays.toString(mass));
     }
-    
+
     static void invertArray() {
     int[] arr = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
-    
-    for (int i = 0; i < arr.length; i++) {
-        System.out.print(arr[i]);
+        System.out.print(Arrays.toString(arr));
+        System.out.println();
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = arr[i] ==1 ? 0 : 1;
+        }
+        System.out.print(Arrays.toString(arr));
+        System.out.println();
     }
-    System.out.println();
-    int n = arr.length;
-    int temp;
-    
-    for (int i = 0; i < n/2; i++) {
-        temp = arr[n-i-1];
-        arr[n-i-1] = arr[i];
-        arr[i] = temp;
-    }
-    for (int i = 0; i < arr.length; i++) {
-        System.out.print(arr[i]);
-    }
-    System.out.println();
-    }
-    
+
     static void fillArray() {
     int[] massiv = new int [100];
-    for (int i = 1; i < massiv.length; i++) {
-        massiv[i] = i;
+    for (int i = 0; i < massiv.length; i++) {
+        massiv[i] = i+1;
         System.out.print(massiv[i]);
     }
     System.out.println();
     }
-    
+
     static void multiplyNumbersInArray() {
     int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i < arr.length; i++) {
@@ -56,9 +46,8 @@ class HomeWorkThree {
     }
     System.out.print(Arrays.toString(arr));
     System.out.println();
-    } 
-    
-    
+    }
+
     static void fillDiagonal() {
     int[][] table = new int[3][3];
     for (int i = 0; i < table.length; i++) { 
@@ -70,6 +59,7 @@ class HomeWorkThree {
             System.out.print("\n");
     }
     }
+
     static int[]retArr(int len, int initialValue) {
         int[] mass = new int [len];
         for (int i = 0; i < mass.length; i++) {
